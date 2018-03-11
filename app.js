23a3ad58f39b9eb148634f8bd71acc750997be76
Fabienmrqs <fabienmrqs@gmail.com>
2018-03-11 19:27:51 +0100

    avancement du jeu

--- app.js
+++ app.js
@@ -1,21 +1,62 @@
-var btn = document.querySelector('.Startbutton')
-var flag= document.querySelectorAll('.flag img')
+const btn = document.querySelector('.Startbutton');
+const flag = document.querySelectorAll('.flag img');
+const timeEl = document.getElementById('timer');
+const start = document.getElementById('game-start');
+const gameover = document.getElementById('game-over');
+let countryfind;
+let countries = [];
 
+refreshflags();
+btn.addEventListener('click', function() {
+  startGame();
+});
+
+function startGame() {
+  setInterval(() => {
+    let time = timeEl.innerText;
+    if (time === '0') {
+      gameover.classList.add('is-open');
+    } else {
+      timeEl.innerHTML = time - 1;
+    }
+  }, 1000);
+
+  start.classList.remove('is-open');
+}
+
+function refreshflags() {
+  countries = [];
+
+  for (let i = 0; i < flag.length; i++) {
+    const flagIndex = Math.floor(Math.random() * flags.length);
+    flag[i].setAttribute('src', 'flags/' + flags[flagIndex].code + '.svg');
+    countries.push(flags[flagIndex]);
+    flag[i].addEventListener('click', doClick);
+  }
+
+  const i = Math.floor(Math.random() * flag.length);
+  countryfind = countries[i];
+  const titlecountries = document.getElementById('nameflag');
+  titlecountries.innerHTML = countryfind.name;
+}
+
+function doClick(event) {
+  const name = event.target
+    .getAttribute('src')
+    .split('/')[1]
+    .split('.')[0];
+
+  if (name === countryfind.code) {
+    timeEl.innerHTML = +timeEl.innerText + 4;
 
     for (let i = 0; i < flag.length; i++) {
-        flag[i].setAttribute('src', "flags/"+flags[Math.floor(Math.random() * flags.length)
-        ].code+".svg")
-        
+      flag[i].removeEventListener('click', doClick);
     }
 
-btn.addEventListener('click', function() {
-    var start = document.getElementById('game-start');
-    var time = document.getElementById('timer')
+    refreshflags();
 
-    
-
-    start.classList.remove("is-open")
-
-
-}); 
-
+    console.log('trouver');
+  } else {
+    console.log('perdu fdp');
+  }
+}
